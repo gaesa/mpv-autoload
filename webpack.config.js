6c06970b766c3064b1bdca7278ae4267d2efba92
gaesa <71256557+gaesa@users.noreply.github.com>
2023-10-22 17:29:25 +0800

    chore: introduce dependencies

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  target: ["web", "es5"],
+  entry: "./src/autoload.ts",
+  output: {
+    filename: "autoload.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
