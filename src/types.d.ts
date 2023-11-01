6bf0abdc63c5d78947e5c117c29445f3088acd02
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-01 12:31:13 +0800

    chore: suppress diagnostic info

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,7 @@
 declare var mp: any;
 declare var dump: (arg: any) => string;
+
+declare module "core-js/es/string/starts-with";
+declare module "core-js/es/string/ends-with";
+declare module "core-js/es/string/trim-end";
+declare module "core-js/es/set";
