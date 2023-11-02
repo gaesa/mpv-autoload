f1e266269fb5d5c41428c68511b7a2ffd7825792
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-02 14:30:35 +0800

    style: reformat

--- src/autoload.ts
+++ src/autoload.ts
@@ -7,284 +7,291 @@ const utils = mp.utils;
 const msg = mp.msg;
 
 function sorted(
-  list: any[],
-  key: (arg: any) => any = function (x) {
-    return x;
-  },
+    list: any[],
+    key: (arg: any) => any = function (x) {
+        return x;
+    },
 ) {
-  return list.slice().sort((a, b) => {
-    const [keyA, keyB] = [key(a), key(b)];
-    if (Array.isArray(keyA) && Array.isArray(keyB)) {
-      for (let i = 0; i < keyA.length && i < keyB.length; i++) {
-        if (keyA[i] < keyB[i]) {
-          return -1;
-        } else if (keyA[i] > keyB[i]) {
-          return 1;
+    return list.slice().sort((a, b) => {
+        const [keyA, keyB] = [key(a), key(b)];
+        if (Array.isArray(keyA) && Array.isArray(keyB)) {
+            for (let i = 0; i < keyA.length && i < keyB.length; i++) {
+                if (keyA[i] < keyB[i]) {
+                    return -1;
+                } else if (keyA[i] > keyB[i]) {
+                    return 1;
+                }
+            }
+            if (keyA.length < keyB.length) {
+                return -1;
+            } else if (keyA.length === keyB.length) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            if (keyA < keyB) {
+                return -1;
+            } else if (keyA === keyB) {
+                return 0;
+            } else {
+                return 1;
+            }
         }
-      }
-      if (keyA.length < keyB.length) {
-        return -1;
-      } else if (keyA.length === keyB.length) {
-        return 0;
-      } else {
-        return 1;
-      }
-    } else {
-      if (keyA < keyB) {
-        return -1;
-      } else if (keyA === keyB) {
-        return 0;
-      } else {
-        return 1;
-      }
-    }
-  });
+    });
 }
 
 function natsort(strings: string[]): string[] {
-  function isDigit(n: string) {
-    return /^\d+$/.test(n);
-  }
+    function isDigit(n: string) {
+        return /^\d+$/.test(n);
+    }
 
-  function key(s: string) {
-    const splitList = s.split(/(\d+)/);
-    splitList[0] === "" ? splitList.shift() : void 0;
-    splitList.length > 0 && splitList[splitList.length - 1] === ""
-      ? splitList.pop()
-      : void 0;
-    return splitList.map((text: string) => {
-      return isDigit(text) ? parseInt(text, 10) : text;
-    });
-  }
+    function key(s: string) {
+        const splitList = s.split(/(\d+)/);
+        splitList[0] === "" ? splitList.shift() : void 0;
+        splitList.length > 0 && splitList[splitList.length - 1] === ""
+            ? splitList.pop()
+            : void 0;
+        return splitList.map((text: string) => {
+            return isDigit(text) ? parseInt(text, 10) : text;
+        });
+    }
 
-  return sorted(strings, key);
+    return sorted(strings, key);
 }
 
 function subprocess(args: string[], check: boolean = false) {
-  const p = mp.command_native({
-    args: args,
-    name: "subprocess",
-    playback_only: false,
-    capture_stdout: true,
-  });
-  if (check) {
-    const status = p.status as number;
-    if (status === 0) {
-      return p;
+    const p = mp.command_native({
+        args: args,
+        name: "subprocess",
+        playback_only: false,
+        capture_stdout: true,
+    });
+    if (check) {
+        const status = p.status as number;
+        if (status === 0) {
+            return p;
+        } else {
+            throw new Error(
+                p.stderr +
+                    "Command " +
+                    JSON.stringify(args) +
+                    " returned non-zero exit status " +
+                    JSON.stringify(status),
+            );
+        }
     } else {
-      throw new Error(
-        p.stderr +
-          "Command " +
-          JSON.stringify(args) +
-          " returned non-zero exit status " +
-          JSON.stringify(status),
-      );
+        return p;
     }
-  } else {
-    return p;
-  }
 }
 
 function splitExt(path: string): [string, string] {
-  const [dir, file] = utils.split_path(path) as [string, string];
-  const lastDotIndex = file.lastIndexOf(".");
-  if (lastDotIndex === 0) {
-    return [path, ""];
-  } else {
-    return [
-      utils.join_path(dir, file.slice(0, lastDotIndex)),
-      file.slice(lastDotIndex),
-    ];
-  }
+    const [dir, file] = utils.split_path(path) as [string, string];
+    const lastDotIndex = file.lastIndexOf(".");
+    if (lastDotIndex === 0) {
+        return [path, ""];
+    } else {
+        return [
+            utils.join_path(dir, file.slice(0, lastDotIndex)),
+            file.slice(lastDotIndex),
+        ];
+    }
 }
 
 function getOS() {
-  function isdir(file: string): boolean {
-    const info = utils.file_info(file);
-    if (info === void 0) {
-      return false;
-    } else {
-      return info.is_dir;
+    function isdir(file: string): boolean {
+        const info = utils.file_info(file);
+        if (info === void 0) {
+            return false;
+        } else {
+            return info.is_dir;
+        }
     }
-  }
 
-  function detectNonWindows() {
-    const unameOutput = subprocess(["uname", "-s"], true).trimEnd() as string;
-    if (unameOutput === "Darwin") {
-      return "darwin";
-    } else if (unameOutput === "Linux") {
-      return "linux";
-    } else {
-      return unameOutput;
+    function detectNonWindows() {
+        const unameOutput = subprocess(
+            ["uname", "-s"],
+            true,
+        ).trimEnd() as string;
+        if (unameOutput === "Darwin") {
+            return "darwin";
+        } else if (unameOutput === "Linux") {
+            return "linux";
+        } else {
+            return unameOutput;
+        }
     }
-  }
 
-  const platform = mp.get_property_native("platform") as string | undefined;
-  if (platform === void 0) {
-    if (utils.getenv("OS") === "Windows_NT") {
-      const HOMEDRIVE = utils.getenv("HOMEDRIVE") as string | undefined;
-      if (HOMEDRIVE !== void 0 && isdir(HOMEDRIVE)) {
-        return "windows";
-      } else {
-        return detectNonWindows();
-      }
+    const platform = mp.get_property_native("platform") as string | undefined;
+    if (platform === void 0) {
+        if (utils.getenv("OS") === "Windows_NT") {
+            const HOMEDRIVE = utils.getenv("HOMEDRIVE") as string | undefined;
+            if (HOMEDRIVE !== void 0 && isdir(HOMEDRIVE)) {
+                return "windows";
+            } else {
+                return detectNonWindows();
+            }
+        } else {
+            return detectNonWindows();
+        }
     } else {
-      return detectNonWindows();
+        return platform;
     }
-  } else {
-    return platform;
-  }
 }
 
 const getMimetype =
-  getOS() === "linux"
-    ? (file: string, extension: string | undefined = void 0) => {
-        const ext = extension === void 0 ? splitExt(file)[1] : extension;
-        const fileArgs = ["file", "-Lb", "--mime-type", "--", file];
-        const args = new Set([".ts", ".bak", ".txt", ".TXT"]).has(ext)
-          ? fileArgs
-          : [
-              "xdg-mime",
-              "query",
-              "filetype",
-              file.startsWith("-") ? "./" + file : file,
-            ];
+    getOS() === "linux"
+        ? (file: string, extension: string | undefined = void 0) => {
+              const ext = extension === void 0 ? splitExt(file)[1] : extension;
+              const fileArgs = ["file", "-Lb", "--mime-type", "--", file];
+              const args = new Set([".ts", ".bak", ".txt", ".TXT"]).has(ext)
+                  ? fileArgs
+                  : [
+                        "xdg-mime",
+                        "query",
+                        "filetype",
+                        file.startsWith("-") ? "./" + file : file,
+                    ];
 
-        const str: string = subprocess(args, true).stdout.trimEnd();
-        const mimeType = str.split("/");
-        if (mimeType.length !== 2) {
-          if (args[0] === "xdg-mime") {
-            const newStr = subprocess(
-              fileArgs,
-              true,
-            ).stdout.trimEnd() as string;
-            const newType = newStr.split("/");
-            if (newType.length !== 2) {
-              throw new Error(JSON.stringify(fileArgs) + " returns: " + newStr);
-            } else {
-              return newType as [string, string];
-            }
-          } else {
-            throw new Error(JSON.stringify(fileArgs) + " returns: " + str);
+              const str: string = subprocess(args, true).stdout.trimEnd();
+              const mimeType = str.split("/");
+              if (mimeType.length !== 2) {
+                  if (args[0] === "xdg-mime") {
+                      const newStr = subprocess(
+                          fileArgs,
+                          true,
+                      ).stdout.trimEnd() as string;
+                      const newType = newStr.split("/");
+                      if (newType.length !== 2) {
+                          throw new Error(
+                              JSON.stringify(fileArgs) + " returns: " + newStr,
+                          );
+                      } else {
+                          return newType as [string, string];
+                      }
+                  } else {
+                      throw new Error(
+                          JSON.stringify(fileArgs) + " returns: " + str,
+                      );
+                  }
+              } else {
+                  return mimeType as [string, string];
+              }
           }
-        } else {
-          return mimeType as [string, string];
-        }
-      }
-    : (file: string, _: string | undefined = void 0) => {
-        // `file` command on Windows:
-        // https://github.com/julian-r/file-windows
-        // note: `-L` option isn't supported in this version
-        const args = ["file", "-b", "--mime-type", "--", file];
-        const str: string = subprocess(args, true).stdout.trimEnd();
-        const mimeType = str.split("/");
-        if (mimeType.length !== 2) {
-          throw new Error(JSON.stringify(args) + " returns: " + str);
-        } else {
-          return mimeType as [string, string];
-        }
-      };
+        : (file: string, _: string | undefined = void 0) => {
+              // `file` command on Windows:
+              // https://github.com/julian-r/file-windows
+              // note: `-L` option isn't supported in this version
+              const args = ["file", "-b", "--mime-type", "--", file];
+              const str: string = subprocess(args, true).stdout.trimEnd();
+              const mimeType = str.split("/");
+              if (mimeType.length !== 2) {
+                  throw new Error(JSON.stringify(args) + " returns: " + str);
+              } else {
+                  return mimeType as [string, string];
+              }
+          };
 
 function unionSet(...sets: Set<any>[]): Set<any> {
-  const mergedSet = new Set<any>();
-  sets.forEach((set) => {
-    set.forEach((elem) => {
-      mergedSet.add(elem);
+    const mergedSet = new Set<any>();
+    sets.forEach((set) => {
+        set.forEach((elem) => {
+            mergedSet.add(elem);
+        });
     });
-  });
-  return mergedSet;
+    return mergedSet;
 }
 
 function getFiles(dir: string, joinFlag: boolean = false): string[] {
-  const allowedTypes = new Set(["video", "audio"]);
-  const commonVideo = new Set([".mp4", ".mkv", ".webm"]);
-  const commonAudio = new Set([".mp3", ".flac"]);
-  const commonMedia = unionSet(commonVideo, commonAudio) as Set<string>;
+    const allowedTypes = new Set(["video", "audio"]);
+    const commonVideo = new Set([".mp4", ".mkv", ".webm"]);
+    const commonAudio = new Set([".mp3", ".flac"]);
+    const commonMedia = unionSet(commonVideo, commonAudio) as Set<string>;
 
-  const files = utils.readdir(dir, "files") as string[];
-  const toBeFiltered = joinFlag
-    ? files.map((file: string) => {
-        return utils.join_path(dir, file) as string;
-      })
-    : files;
-  return natsort(
-    toBeFiltered.filter((file: string) => {
-      const ext = splitExt(file)[1];
-      if (commonMedia.has(ext)) {
-        return true;
-      } else {
-        return allowedTypes.has(getMimetype(file, ext)[0]);
-      }
-    }),
-  );
+    const files = utils.readdir(dir, "files") as string[];
+    const toBeFiltered = joinFlag
+        ? files.map((file: string) => {
+              return utils.join_path(dir, file) as string;
+          })
+        : files;
+    return natsort(
+        toBeFiltered.filter((file: string) => {
+            const ext = splitExt(file)[1];
+            if (commonMedia.has(ext)) {
+                return true;
+            } else {
+                return allowedTypes.has(getMimetype(file, ext)[0]);
+            }
+        }),
+    );
 }
 
 function checkPlaylist(pl_count: number) {
-  if (pl_count > 1) {
-    msg.verbose("Stopping: manually made playlist");
-    return false;
-  } else {
-    return true;
-  }
+    if (pl_count > 1) {
+        msg.verbose("Stopping: manually made playlist");
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function fdCurrentEntryPos(files: string[], file: string) {
-  const current = files.indexOf(file);
-  if (current === -1) {
-    return void 0;
-  } else {
-    msg.trace("current file position in files: " + current);
-    return current;
-  }
+    const current = files.indexOf(file);
+    if (current === -1) {
+        return void 0;
+    } else {
+        msg.trace("current file position in files: " + current);
+        return current;
+    }
 }
 
 function stripTrailingSlash(path: string) {
-  return path.endsWith("/") && path !== "/" ? path.slice(0, -1) : path;
+    return path.endsWith("/") && path !== "/" ? path.slice(0, -1) : path;
 }
 
 function splitPath(path: string): [string, string] {
-  const [dir, file] = utils.split_path(path) as [string, string];
-  return [stripTrailingSlash(dir), file];
+    const [dir, file] = utils.split_path(path) as [string, string];
+    return [stripTrailingSlash(dir), file];
 }
 
 function addFilesToPlaylist(files: string[], current: number) {
-  files.splice(current, 1);
-  files.forEach((file: string) => {
-    mp.command_native(["loadfile", file, "append"]);
-  });
-  mp.command_native(["playlist-move", 0, current + 1]);
+    files.splice(current, 1);
+    files.forEach((file: string) => {
+        mp.command_native(["loadfile", file, "append"]);
+    });
+    mp.command_native(["playlist-move", 0, current + 1]);
 }
 
 function main() {
-  const path: string | undefined = mp.get_property_native("path");
-  if (path === void 0) {
-    msg.warn("Fail to get the path of the currently played file");
-  } else {
-    let [dir, file] = splitPath(path);
-    const joinFlag = utils.getcwd() !== dir;
-    file = joinFlag ? path : file;
-
-    const files = getFiles(dir, joinFlag);
-    if (files.length === 0) {
-      msg.verbose("No other video or audio files in the directory");
+    const path: string | undefined = mp.get_property_native("path");
+    if (path === void 0) {
+        msg.warn("Fail to get the path of the currently played file");
     } else {
-      const current = fdCurrentEntryPos(files, file);
-      if (current === void 0) {
-        msg.warn(
-          "Can't find the position of the currently played file in media files",
-        );
-      } else {
-        addFilesToPlaylist(files, current);
-      }
+        let [dir, file] = splitPath(path);
+        const joinFlag = utils.getcwd() !== dir;
+        file = joinFlag ? path : file;
+
+        const files = getFiles(dir, joinFlag);
+        if (files.length === 0) {
+            msg.verbose("No other video or audio files in the directory");
+        } else {
+            const current = fdCurrentEntryPos(files, file);
+            if (current === void 0) {
+                msg.warn(
+                    "Can't find the position of the currently played file in media files",
+                );
+            } else {
+                addFilesToPlaylist(files, current);
+            }
+        }
     }
-  }
 }
 
 mp.register_event("start-file", () => {
-  const pl_count = mp.get_property_native("playlist-count", 1) as number;
-  if (checkPlaylist(pl_count)) {
-    main();
-  } else {
-    return;
-  }
+    const pl_count = mp.get_property_native("playlist-count", 1) as number;
+    if (checkPlaylist(pl_count)) {
+        main();
+    } else {
+        return;
+    }
 });
