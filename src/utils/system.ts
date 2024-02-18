4553a3af9c29cf61d13ccccdcb2ba91f7369796e
gaesa <71256557+gaesa@users.noreply.github.com>
2024-02-19 01:55:14 +0800

    style: inline if

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -2,9 +2,5 @@ import * as Paths from "./paths";
 
 export function isWindows(): boolean {
     const platform = mp.get_property("platform");
-    if (platform !== void 0) {
-        return platform === "windows";
-    } else {
-        return Paths.exists("\\");
-    }
+    return platform !== void 0 ? platform === "windows" : Paths.exists("\\");
 }
