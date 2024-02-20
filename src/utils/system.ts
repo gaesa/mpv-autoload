489fae27bac395cd611206077ddf925e4ae000db
gaesa <71256557+gaesa@users.noreply.github.com>
2024-02-20 17:03:12 +0800

    chore: explicitly specify type of the exported variable

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,6 @@
 import * as Paths from "./paths";
 
-export const isWindows = (() => {
+export const isWindows: boolean = (() => {
     const platform = mp.get_property("platform");
     return platform !== void 0
         ? platform === "windows"
