a508f9efef33b391a3be02579b44a79743bdf2a2
gaesa <71256557+gaesa@users.noreply.github.com>
2024-02-20 13:44:25 +0800

    feat: more reliable platform detection

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -2,5 +2,7 @@ import * as Paths from "./paths";
 
 export const isWindows = (() => {
     const platform = mp.get_property("platform");
-    return platform !== void 0 ? platform === "windows" : Paths.exists("\\");
+    return platform !== void 0
+        ? platform === "windows"
+        : Paths.exists("\\") && !Paths.exists("//");
 })();
